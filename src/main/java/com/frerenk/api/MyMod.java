3b503954287dc6a3e4def21abda9bc337824cb29
FRErenK <frerenk777001@gmail.com>
2026-07-06 18:31:07 +0300

    Create MyMod.java

---
+++ src/main/java/com/frerenk/api/MyMod.java
@@ -0,0 +1,10 @@
+package com.frerenk.api;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MyMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Modum başarıyla yüklendi!");
+    }
+}
